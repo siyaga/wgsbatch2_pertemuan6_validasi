d25b21478c994fbaa0e2085c9ff217902413a86b
lifeforme <siyaga123@gmail.com>
2022-07-11 13:57:02 +0700

    fixed membuat output 1x keluar

--- contact.js
+++ contact.js
@@ -21,27 +21,6 @@ const readline = require('readline');
   }
   }
 
-  //Membuat fungsi Validasi Email
-  function validasiEmail (email){
-    if(!validator.isEmail(email)){
-      return console.log("Email yang anda Masukan Salah!, Pastikan Format email sesuai.");
-    }else {
-      console.log("Email yang anda masukan benar");
-    }
-    
-
-  }
-
-  //Membuat fungsi Validasi Number
-  function validasiMobile(mobile){
-    if(!validator.isMobilePhone(mobile, 'id-ID')){
-      return console.log("Nomor Telephone Yang anda Masukan Salah!!, Pastikan Format nomor sesuai.");
-    
-    } else {
-      console.log("Nomor yang anda masukan Benar");
-    }
-    
-  }
 
 //Simpan Data
 const saveIsiData = (name,mobile,email) => {
@@ -51,19 +30,26 @@ const saveIsiData = (name,mobile,email) => {
     const file = fs.readFileSync('data/contacts.json', 'utf8');
     const contacts =JSON.parse(file);
     // Membuat Jika Nama duplikat
-
-    const namaDuplikat = contacts.find((contact) => contact.name.toLowerCase() === name.toLowerCase());
-
-    //Menambahkan validasi untuk nomor telephone
-    validasiMobile(mobile);
-    //Menambahkan validasi untuk email
-    validasiEmail(email);
-
+const namaDuplikat = contacts.find((contact) => contact.name.toLowerCase() === name.toLowerCase());
     if(namaDuplikat){
-        //Menampilkan jika nilai true bila sama namanya maka munculkan nama sudah digunakan
-        //agar error berhenti disini
-        return  console.log("Nama sudah digunakan, silakan ganti nama lain");
+      //Menampilkan jika nilai true bila sama namanya maka munculkan nama sudah digunakan
+      //agar error berhenti disini
+      return  console.log("Nama sudah digunakan, silakan ganti nama lain");
+  }
+     //Menambahkan validasi untuk nomor telephone
+     if(!validator.isMobilePhone(mobile, 'id-ID')){
+      console.log("Nomor Telephone Yang anda Masukan Salah!!, Pastikan Format nomor sesuai.")
+      return false;
+    
     }
+    //Menambahkan validasi untuk email
+    if(!validator.isEmail(email)){
+      console.log("Email yang anda Masukan Salah!, Pastikan Format email sesuai.")
+      return false;
+    }
+    
+
+
     contacts.push(contact);
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
     console.log("Terima Kasih sudah memasukkan data!");
